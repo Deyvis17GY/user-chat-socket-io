59efb4fc62ef4ee58a3102384b975d47d014c640
Deyvis17 <deyvisjaime17@gmail.com>
2022-03-12 00:58:56 -0500

    🚧 working form user component

---
+++ frontend/src/pages/Home/index.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { TableUSer } from '../../components/TableUser';
+
+export default function Home() {
+  return (
+    <div>
+      <TableUSer />
+    </div>
+  );
+}
