08d7a7a76199656d1a83ae6f04c75e9df2d7687a
Deyvis17 <deyvisjaime17@gmail.com>
2022-03-12 20:14:41 -0500

    feat: add headers

--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -1,5 +1,11 @@
 import axios from 'axios';
 
 export const baseHttps = axios.create({
-  baseURL: import.meta.env.VITE_API_URL
+  baseURL: import.meta.env.VITE_API_URL,
+  headers: {
+    'Content-Type': 'application/json',
+    'Access-Control-Allow-Origin': '*',
+    'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE, PATCH, OPTIONS',
+    'Access-Control-Allow-Headers': 'Origin, X-Requested-With, Content-Type, Accept'
+  }
 });
